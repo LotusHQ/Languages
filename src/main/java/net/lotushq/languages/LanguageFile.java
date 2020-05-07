005b8c1152f11cf0dbf27d12f76335df0930bab1
Jatyn Stacy <jlee0964@gmail.com>
2020-05-06 22:55:58 -0400

    Add support for possessive nouns.

--- src/main/java/net/lotushq/languages/LanguageFile.java
+++ src/main/java/net/lotushq/languages/LanguageFile.java
@@ -10,18 +10,21 @@ public abstract class LanguageFile extends YamlConfiguration implements Cloneabl
 
     /**
      * Gets the String that is placed in front of a prefixed message.
+     *
      * @return the prefix
      */
     public abstract String prefix();
 
     /**
      * Gets the String that is defaulted to when an input is null.
+     *
      * @return the default message
      */
     public abstract String unknown();
 
     /**
      * Prefixes and colors the string at the given path, but the message begins with {@link ChatColor#RED}
+     *
      * @param message the raw message
      * @return the prefixed and colored message, beginning with the color RED until it is overwritten.
      */
@@ -33,7 +36,8 @@ public abstract class LanguageFile extends YamlConfiguration implements Cloneabl
 
     /**
      * Translates the '&' alternate color code symbol.
-     * @param path the path to the raw message in the configuration
+     *
+     * @param message the raw text
      * @return the colored message
      */
     public String color(String message) {
@@ -44,7 +48,8 @@ public abstract class LanguageFile extends YamlConfiguration implements Cloneabl
 
     /**
      * Appends the prefix and translates color codes.
-     * @param path the path to the String in the configuration
+     *
+     * @param message the raw message
      * @return the message prefixed and colored.
      */
     public String titled(String message) {
@@ -52,6 +57,22 @@ public abstract class LanguageFile extends YamlConfiguration implements Cloneabl
                 .map(prefix()::concat)
                 .map(this::color)
                 .orElse(unknown());
+    }
+
+    /**
+     * Transforms a name to show possession. By default this method only
+     * supports adding an apostrophe (and an s, if applicable), as in an
+     * English Locale.
+     * <p>
+     * Whether or not apostrophe possession is used is based on the
+     * "apostrophe_possession" boolean value in the LanguageFile.
+     *
+     * @param name the name to edit
+     * @return the name, in possessive form if applicable.
+     */
+    public String possessive(String name) {
+        return !getBoolean("apostrophe_possession", true) ? name :
+                (name.endsWith("s") ? name + "'" : name + "'s");
     }
 
     static <T extends LanguageFile> T empty(Class<T> clazz) {
